12d9e3be12089a27d2287acc99c8584d705bb06a
lsavkova <40965045+lsavkova@users.noreply.github.com>
2018-07-12 21:53:02 +0200

    Initial commit

---
+++ app/src/main/java/no/lolhq/lekse3/MainActivity.java
@@ -0,0 +1,6 @@
+package no.lolhq.lekse3;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
---
+++ app/src/main/java/no/lolhq/lekse3/MainActivityLekse.java
@@ -0,0 +1,13 @@
+package no.lolhq.lekse3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityLekse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_lekse);
+    }
+}
